bde437f77abafbdbc8a971bdca50d9670c211fab
Hector Astrom <hector.astrom@gmail.com>
2024-08-05 16:22:33 +0200

    updated api url

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from app.helpers.similarity import compute_similarity
 from app.routes import concepts, users
@@ -8,29 +8,30 @@ app = FastAPI()
 
 if PRODUCTION:
     origins = ["circalearn.net"]  # domain-to-be
-elif not PRODUCTION:
-    origins = ['*']
+else:
+    origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
-# Include API Routes
-app.include_router(concepts.router, prefix="/api/v1", tags=['concepts'])
-app.include_router(users.router, prefix="/api/v1", tags=["users"])
+# Create a main API router and add all other routers to it
+api_router = APIRouter()
+api_router.include_router(concepts.router, tags=["concepts"])
+api_router.include_router(users.router, tags=["users"])
 
 
-@app.get("/")
+# Routes currently for testing -- still at "/api/v1/" not just "/"
+@api_router.get("/")
 def root():
     return {"message": "Welcome to the Circa API"}
 
 
-# Route to quickly compare two sentences
-@app.get("/compare/{ref}/{other}")
+@api_router.get("/compare/{ref}/{other}")
 def compare(ref: str, other: str):
     # Turn dashes "-" into spaces " "
     ref = " ".join(ref.split("-"))
@@ -39,9 +40,13 @@ def compare(ref: str, other: str):
     return {"similarity": similarity}
 
 
+# Include the complete api router in the FastAPI app with the prefix /api/v1
+app.include_router(api_router, prefix="/api/v1")
+
 # For fast local development
 # Use `uvicorn app.main:app --reload` to start
 # For production, we will need to use Uvicorn
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
